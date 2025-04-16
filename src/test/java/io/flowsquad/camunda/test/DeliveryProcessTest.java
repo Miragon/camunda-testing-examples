bd659371dbc17a09dc8cbe5ed68cce4c2de7f4e6
Martin Berner <73341627+marber63@users.noreply.github.com>
2025-04-16 06:53:53 +0200

    feat: migrate repo to junit5 and add bpmn-to-code plugin

--- src/test/java/io/flowsquad/camunda/test/DeliveryProcessTest.java
+++ src/test/java/io/flowsquad/camunda/test/DeliveryProcessTest.java
@@ -1,89 +1,70 @@
 package io.flowsquad.camunda.test;
 
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
-import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.camunda.bpm.scenario.ProcessScenario;
 import org.camunda.bpm.scenario.Scenario;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.camunda.community.process_test_coverage.junit5.platform7.ProcessEngineCoverageExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
+import static io.flowsquad.camunda.test.DeliveryprocessProcessApiV1.Elements.EndEvent_DeliveryCompleted;
+import static io.flowsquad.camunda.test.DeliveryprocessProcessApiV1.Elements.Task_DeliverOrder;
+import static io.flowsquad.camunda.test.DeliveryprocessProcessApiV1.PROCESS_ID;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.withVariables;
 import static org.mockito.Mockito.*;
 
-@Deployment(resources = "deliver-process.bpmn")
-public class DeliveryProcessTest {
+@Deployment(resources = "delivery-process.bpmn")
+@ExtendWith(ProcessEngineCoverageExtension.class)
+class DeliveryProcessTest {
 
-    public static final String PROCESS_KEY = "deliveryprocess";
-    public static final String TASK_DELIVER_ORDER = "Task_DeliverOrder";
     public static final String VAR_ORDER_DELIVERED = "orderDelivered";
-    public static final String END_EVENT_DELIVERY_COMPLETED = "EndEvent_DeliveryCompleted";
-    public static final String END_EVENT_DELIVERY_CANCELLED = "EndEvent_DeliveryCancelled";
 
-    @Rule
-    @ClassRule
-    public static TestCoverageProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create()
+    @SuppressWarnings("unused")
+    public static ProcessEngineCoverageExtension extension = ProcessEngineCoverageExtension
+            .builder()
             .assertClassCoverageAtLeast(0.9)
             .build();
 
     @Mock
     private ProcessScenario testDeliveryProcess;
 
-    @Before
+    @BeforeEach
     public void defaultScenario() {
         MockitoAnnotations.initMocks(this);
 
         //Happy-Path
-        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER))
-                .thenReturn(task -> {
-                    task.complete(withVariables(VAR_ORDER_DELIVERED, true));
-                });
+        when(testDeliveryProcess.waitsAtUserTask(Task_DeliverOrder))
+                .thenReturn(task -> task.complete(withVariables(VAR_ORDER_DELIVERED, true)));
     }
 
-    @Test
-    public void shouldExecuteHappyPath() {
-        Scenario.run(testDeliveryProcess)
-                .startByKey(PROCESS_KEY)
-                .execute();
-
-        verify(testDeliveryProcess)
-                .hasFinished(END_EVENT_DELIVERY_COMPLETED);
-    }
-
-    @Test
-    public void shouldExecuteOrderCancelled() {
-        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER)).thenReturn(task -> {
-            taskService().handleBpmnError(task.getId(), "DeliveryCancelled");
-        });
-
-        Scenario.run(testDeliveryProcess)
-                .startByKey(PROCESS_KEY)
-                .execute();
-
-        verify(testDeliveryProcess)
-                .hasFinished(END_EVENT_DELIVERY_CANCELLED);
-    }
-
+    @DisplayName("Delivery fails")
     @Test
     public void shouldExecuteDeliverTwice() {
-        when(testDeliveryProcess.waitsAtUserTask(TASK_DELIVER_ORDER)).thenReturn(task -> {
-            task.complete(withVariables(VAR_ORDER_DELIVERED, false));
-        }, task -> {
-            task.complete(withVariables(VAR_ORDER_DELIVERED, true));
-        });
+        when(testDeliveryProcess.waitsAtUserTask(Task_DeliverOrder))
+                .thenReturn(task -> task.complete(withVariables(VAR_ORDER_DELIVERED, false)), task -> task.complete(withVariables(VAR_ORDER_DELIVERED, true)));
 
         Scenario.run(testDeliveryProcess)
-                .startByKey(PROCESS_KEY)
+                .startByKey(PROCESS_ID)
                 .execute();
 
         verify(testDeliveryProcess, times(2))
-                .hasCompleted(TASK_DELIVER_ORDER);
+                .hasCompleted(Task_DeliverOrder);
         verify(testDeliveryProcess)
-                .hasFinished(END_EVENT_DELIVERY_COMPLETED);
+                .hasFinished(EndEvent_DeliveryCompleted);
+    }
+
+    @DisplayName("Happy Path")
+    @Test
+    public void shouldExecuteHappyPath() {
+        Scenario.run(testDeliveryProcess)
+                .startByKey(PROCESS_ID)
+                .execute();
+
+        verify(testDeliveryProcess)
+                .hasFinished(EndEvent_DeliveryCompleted);
     }
 }
